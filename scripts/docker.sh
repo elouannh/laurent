edfa1b02f246705cb58f11cb35af0df282dcde4c
elouannh <elouannhosta27@gmail.com>
2024-12-02 14:50:59 +0100

    Added an OS command

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker container stop laurent-$1-omg
 docker container remove laurent-$1-omg
 docker build -t laurent-$1 -f envs/$1/Dockerfile . && \
 docker run --rm --cpuset-cpus="0-7" --name laurent-$1-omg laurent-$1
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <dpp/dpp.h>
 #include <cstdlib>
 #include <fstream>
+#include <sys/utsname.h>
+
+std::string get_system_info() {
+    struct utsname sys_info;
+    if (uname(&sys_info) == 0) {
+        std::string info = "System: " + std::string(sys_info.sysname) + "\n" +
+                           "Name: " + std::string(sys_info.nodename) + "\n" +
+                           "Version: " + std::string(sys_info.release) + "\n" +
+                           "Architecture: " + std::string(sys_info.machine) + "\n";
+        return info;
+    } else {
+        return "Error: NTM.\n";
+    }
+}
 
 std::string get_token(const char *filename) {
     std::ifstream file(filename);
@@ -22,12 +36,17 @@ int main(int argc, char **argv) {
         if (event.command.get_command_name() == "ping") {
             event.reply("Pong!");
         }
+        if (event.command.get_command_name() == "os") {
+            event.reply(get_system_info());
+        }
     });
 
     bot.on_ready([&bot](auto event) {
         if (dpp::run_once<struct register_bot_commands>()) {
             bot.global_command_create(
                 dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+            bot.global_command_create(
+                dpp::slashcommand("os", "Get OS information.", bot.me.id));
         }
     });
     bot.start(dpp::st_wait);
